1d7d765dc7450fb5af8e9acc76224f429984da5a
shrmaaarna <123148550+shrmaaarna@users.noreply.github.com>
2023-04-06 19:18:54 +1000

    Add files via upload

---
+++ MatterJSBoilerPlate-main/MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+	
+	var ball_options={
+		isStatic:false,
+		restitution: 0.3,
+		friction:0,
+		density:1.2
+	}
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
